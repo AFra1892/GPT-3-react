77828a62dfbcf689dbc646d60833491bc4e71ab6
amir <mashhadifarahani1@gmail.com>
2023-09-24 00:13:23 +0330

    finish app.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Footer,Header,WhatGPT3,Blog,Possibility,Features } from './containers'
 import Brand from './components/brand/Brand2'
 import CTA from './components/cta/CTA2'
 import Navbar from './components/navbar/Navbar'
+import './App.css'
+
 const App = () => {
   return (
     <div className='App'>
