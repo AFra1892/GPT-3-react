e2fe28fa54a8fa862394f248737d216bdc4f2fc6
amir <mashhadifarahani1@gmail.com>
2023-10-10 22:39:05 +0330

    deleteing wrong code

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,24 +5,6 @@ import people from '../../assets/people.png'
 
 const Header = () => {
   return (
-//my code
-    // <div className='gpt3__header'>
-    //   <div className='gpt3__header-left-side'>
-    //     <h1 className='gpt3__header-left-side_heading'>Let's Build Something amazing with GPT-3 OpenAI</h1>
-    //     <p className='gpt3__header-left-side_description'>
-    //     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-    //     </p>
-    //     <div className='gpt3__header-left-side_search-box'>
-    //       <input placeholder='Your Email Address' type='text'/>
-    //       <button>Get Started</button>
-    //     </div>
-    //   </div>
-    //   <div className='gpt3__header-right-side'>
-    //     <img src={lo}/>
-    //   </div>
-    // </div>
-
-  
     <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
